10f5801bf419c8fc6888f817ab141a304474c80a
Andreas Thomas <dev@chronark.com>
2022-06-15 10:24:16 +0200

    fix: send headers correctly (#1)

--- main.ts
+++ main.ts
@@ -5,6 +5,30 @@ const q = new Client({
     "eyJVc2VySUQiOiI3ZjBlNGY0ZS1lNGM5LTQ3M2MtOThjYi1mNmEwZjcxZTdmNGUiLCJQYXNzd29yZCI6IjM1M2EzYzQ2LTAwNjYtNDM5Ni1hM2UwLWJlOGUyYzFmMjdmZCJ9",
 });
 
-const res = await q.messages.list();
+const topic = await q.topics.create({ name: crypto.randomUUID() });
+const url = "https://qstash-local.requestcatcher.com/test";
+const endpoint = await q.endpoints.create({ url, topicName: topic.name });
 
-console.log(res);
+console.log({ endpoint });
+
+// for (let i = 0; i <= 1000; i++) {
+//   console.log(
+//     i,
+//     await q.publishJSON({
+//       topic: topic.name,
+//       cron: "* * * * *",
+//       body: { hello: "world" },
+//     }),
+//   );
+// }
+
+let cursor = Date.now();
+const logs: unknown[] = [];
+while (cursor > 0) {
+  console.log({ cursor });
+  const res = await q.logs({ cursor });
+  logs.push(...res.logs);
+  cursor = res.cursor ?? 0;
+}
+
+console.log(logs);
