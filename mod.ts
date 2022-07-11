9969333eadf519ae75f27ba678e35d17d3c8ee01
Andreas Thomas <dev@chronark.com>
2022-07-11 17:43:03 +0200

    fix: import paths

--- mod.ts
+++ mod.ts
@@ -1,7 +1,7 @@
 // dnt-shim-ignore
 
-import * as c from "../pkg/receiver.ts";
-export * from "../pkg/client/client.ts";
+import * as c from "./pkg/receiver.ts";
+export * from "./pkg/client/client.ts";
 
 export class Receiver extends c.Receiver {
   constructor(config: Omit<c.ReceiverConfig, "crypto">) {
