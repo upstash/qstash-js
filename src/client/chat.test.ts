91ba78ceca58283e37723aa4bde6ad7482b6288a
CahidArda <cahidardaooz@hotmail.com>
2024-06-06 23:06:03 +0300

    test: add chat tests

---
+++ src/client/chat.test.ts
@@ -0,0 +1,89 @@
+import { describe, expect, test } from "bun:test";
+import { Client } from "./client";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import type { ChatCompletionChunk } from "./types";
+
+
+async function checkStream(
+  stream: AsyncIterable<ChatCompletionChunk>,
+  expectInStream: string[] // array of strings to expect in stream
+): Promise<void> {
+  const _stream = OpenAIStream(stream)
+  const textResponse = new StreamingTextResponse(_stream)
+  const text = await textResponse.text();
+
+  const lines = text.split("\n").filter(line => line.length > 0);
+  
+  expect(lines.length).toBeGreaterThan(0);
+  expect(lines.some(line => line.startsWith("0:\""))).toBeTrue() // all lines start with `0:"`
+  expect(expectInStream.every(token => text.includes(token))).toBeTrue()
+}
+
+describe("Test Qstash chat", () => {
+  const client = new Client({ token: process.env.QSTASH_TOKEN! });
+
+  test("should respond to prompt", async () => {
+    const response = await client.chat.prompt({
+      model: "meta-llama/Meta-Llama-3-8B-Instruct",
+      system: "from now on, foo is whale",
+      user: "what exactly is foo?"
+    });
+
+    expect(response instanceof ReadableStream).toBeFalse();
+    expect(response.choices.length).toBe(1);
+    expect(response.choices[0].message.content.includes("whale")).toBeTrue();
+    expect(response.choices[0].message.role).toBe("assistant");
+  })
+
+  test("should respond to create", async () => {
+    const response = await client.chat.create({
+      model: "meta-llama/Meta-Llama-3-8B-Instruct",
+      messages: [
+        {
+          role: "system",
+          content: "from now on, foo is whale"
+        },
+        {
+          role: "user",
+          content: "what exactly is foo?"
+        },
+      ],
+    });
+
+    expect(response instanceof ReadableStream).toBeFalse();
+    expect(response.choices.length).toBe(1);
+    expect(response.choices[0].message.content.includes("whale")).toBeTrue();
+    expect(response.choices[0].message.role).toBe("assistant");
+  })
+
+  test("should stream prompt", async () => {
+    const response = await client.chat.prompt({
+      model: "meta-llama/Meta-Llama-3-8B-Instruct",
+      system: "from now on, foo is whale",
+      user: "what exactly is foo?",
+      stream: true
+    });
+
+    await checkStream(response, ["whale"])
+  }, {timeout: 30_000})
+
+  test("should stream create", async () => {
+    const response = await client.chat.create({
+      model: "meta-llama/Meta-Llama-3-8B-Instruct",
+      messages: [
+        {
+          role: "system",
+          content: "from now on, foo is whale"
+        },
+        {
+          role: "user",
+          content: "what exactly is foo?"
+        },
+      ],
+      stream: true,
+      max_tokens: 5
+    });
+
+    await checkStream(response, ["whale"])
+  })
+})
