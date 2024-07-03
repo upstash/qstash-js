6e407f0d476c059c58510a2689372b35d7eedac5
CahidArda <cahidardaooz@hotmail.com>
2024-07-03 12:39:02 +0300

    feat: add qstash/workflow import

--- tsup.config.js
+++ tsup.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: { index: "./src/index.ts", nextjs: "./src/nextjs.ts" },
+  entry: {
+    index: "./src/index.ts",
+    nextjs: "./src/nextjs.ts",
+    workflow: "./src/client/workflow/index.ts",
+  },
   format: ["cjs", "esm"],
   splitting: true,
   sourcemap: false,
