4cef4e6837b105e310f5927e0d20d7d41a6e5cc8
ogzhanolguncu <ogzhan11@gmail.com>
2024-07-05 11:52:25 +0300

    feat: add error cause

--- src/client/error.ts
+++ src/client/error.ts
@@ -4,20 +4,21 @@ import type { ChatRateLimit, RateLimit } from "./types";
  * Result of 500 Internal Server Error
  */
 export class QstashError extends Error {
-  constructor(message: string) {
+  constructor(message: string, cause?: unknown) {
     super(message);
     this.name = "QstashError";
+    this.cause = cause;
   }
 }
 
 export class QstashRatelimitError extends QstashError {
   constructor(args: RateLimit) {
-    super(`You have been ratelimited. ${JSON.stringify(args)} `);
+    super(`You have been ratelimited. ${JSON.stringify(args)} `, args);
   }
 }
 
 export class QstashChatRatelimitError extends QstashError {
   constructor(args: ChatRateLimit) {
-    super(`You have been ratelimited. ${JSON.stringify(args)} `);
+    super("You have been ratelimited.", args);
   }
 }
