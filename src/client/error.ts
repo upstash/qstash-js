ae1a9d2543ca8b901ce46811be93f5f745f65ede
CahidArda <cahidardaooz@hotmail.com>
2024-07-16 11:44:22 +0300

    feat: add public fields to qstash errors

--- src/client/error.ts
+++ src/client/error.ts
@@ -11,19 +11,46 @@ export class QstashError extends Error {
 }
 
 export class QstashRatelimitError extends QstashError {
+  public limit: string | null;
+  public remaining: string | null;
+  public reset: string | null;
+
   constructor(args: RateLimit) {
     super(`Exceeded burst rate limit. ${JSON.stringify(args)} `);
+    this.limit = args.limit;
+    this.remaining = args.remaining;
+    this.reset = args.reset;
   }
 }
 
 export class QstashChatRatelimitError extends QstashError {
+  public limitRequests: string | null;
+  public limitTokens: string | null;
+  public remainingRequests: string | null;
+  public remainingTokens: string | null;
+  public resetRequests: string | null;
+  public resetTokens: string | null;
+
   constructor(args: ChatRateLimit) {
     super(`Exceeded chat rate limit. ${JSON.stringify(args)} `);
+    this.limitRequests = args["limit-requests"];
+    this.limitTokens = args["limit-tokens"];
+    this.remainingRequests = args["remaining-requests"];
+    this.remainingTokens = args["remaining-tokens"];
+    this.resetRequests = args["reset-requests"];
+    this.resetTokens = args["reset-tokens"];
   }
 }
 
 export class QstashDailyRatelimitError extends QstashError {
+  public limit: string | null;
+  public remaining: string | null;
+  public reset: string | null;
+
   constructor(args: RateLimit) {
     super(`Exceeded daily rate limit. ${JSON.stringify(args)} `);
+    this.limit = args.limit;
+    this.remaining = args.remaining;
+    this.reset = args.reset;
   }
 }
