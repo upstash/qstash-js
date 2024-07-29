82687ed705ec061e6facfd5f6ed17a3203bd2ee8
CahidArda <cahidardaooz@hotmail.com>
2024-07-29 18:27:03 +0300

    feat: create platforms folder and mv nextjs into it

--- tsup.config.js
+++ tsup.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: { index: "./src/index.ts", nextjs: "./src/nextjs.ts" },
+  entry: {
+    index: "./src/index.ts",
+    nextjs: "./platforms/nextjs.ts",
+    nuxt: "./platforms/nuxt.ts",
+  },
   format: ["cjs", "esm"],
   splitting: true,
   sourcemap: false,
