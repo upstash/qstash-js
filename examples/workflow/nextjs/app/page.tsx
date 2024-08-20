1bd7906b19e6a38638feda3dc7f2cd001c82bc43
CahidArda <cahidardaooz@hotmail.com>
2024-08-20 10:43:37 +0300

    fix: rm baseUrl from form

--- examples/workflow/nextjs/app/page.tsx
+++ examples/workflow/nextjs/app/page.tsx
@@ -4,17 +4,9 @@ import { useSearchParams } from 'next/navigation';
 import { Suspense, useEffect, useState } from 'react';
 
 function Home() {
-  const [baseUrl, setBaseUrl] = useState("http://localhost:3000");
   const [requestBody, setRequestBody] = useState('{"date":123,"email":"my@mail.com","amount":10}');
   const [loading, setLoading] = useState(false);
   const searchParams = useSearchParams();
-
-  // Ensure baseUrl doesn't have a trailing slash
-  useEffect(() => {
-    if (baseUrl.endsWith('/')) {
-      setBaseUrl(baseUrl.replace(/\/$/, ''));
-    }
-  }, [baseUrl]);
 
   const search = searchParams.get('function');
   const [route, setRoute] = useState(search ?? "path");
@@ -23,7 +15,7 @@ function Home() {
 
   const handleSend = async () => {
     setLoading(true);
-    const url = `${baseUrl}/-call-qstash`;
+    const url = `/-call-qstash`;
     try {
       const response = await fetch(url, {
         headers: {
@@ -31,7 +23,6 @@ function Home() {
         },
         method: "POST",
         body: JSON.stringify({
-          baseUrl,
           route,
           payload: JSON.parse(requestBody)
         })
@@ -48,16 +39,6 @@ function Home() {
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-6 rounded shadow-md w-full max-w-md">
         <h1 className="text-xl font-bold mb-4">Send Request</h1>
-
-        <div className="mb-4">
-          <label className="block text-gray-700">Base URL (replace with deployment URL):</label>
-          <input
-            type="text"
-            value={baseUrl}
-            onChange={(e) => setBaseUrl(e.target.value)}
-            className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-        </div>
 
         <div className="mb-4">
           <label className="block text-gray-700">Route:</label>
