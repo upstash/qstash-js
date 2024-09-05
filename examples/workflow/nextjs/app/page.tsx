fc58fa3f2eef35766135cf2b9ad462d89191f2bb
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-09-06 00:29:50 +0300

    update example ui

--- examples/workflow/nextjs/app/page.tsx
+++ examples/workflow/nextjs/app/page.tsx
@@ -1,7 +1,8 @@
 "use client"
 
+import Image from 'next/image';
 import { useSearchParams } from 'next/navigation';
-import { Suspense, useEffect, useState } from 'react';
+import { Suspense, useState } from 'react';
 
 function Home() {
   const [requestBody, setRequestBody] = useState('{"date":123,"email":"my@mail.com","amount":10}');
@@ -36,43 +37,74 @@ function Home() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-6 rounded shadow-md w-full max-w-md">
-        <h1 className="text-xl font-bold mb-4">Send Request</h1>
-
-        <div className="mb-4">
-          <label className="block text-gray-700">Route:</label>
-          <select
-            value={route}
-            onChange={(e) => setRoute(e.target.value)}
-            className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          >
-            <option value="" disabled>Select route</option>
-            {routes.map((r) => (
-              <option key={r} value={r}>
-                {r}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        <div className="mb-4">
-          <label className="block text-gray-700">Request Body:</label>
-          <textarea
-            value={requestBody}
-            onChange={(e) => setRequestBody(e.target.value)}
-            className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-        </div>
-
-        <button
-          onClick={handleSend}
-          disabled={loading}
-          className={`w-full px-4 py-2 text-white rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 ${loading ? 'bg-gray-500' : 'bg-blue-500 hover:bg-blue-600'}`}
-        >
-          {loading ? 'Sending...' : 'Send'}
-        </button>
+    <div className='flex flex-col justify-between h-screen '>
+      <div className='flex items-center justify-center w-full h-20 text-white bg-black'>
+        <Image src="/upstash-dark-bg.svg" alt='upstash-logo' width={125} height={100}/>
       </div>
+      <div className="flex flex-row items-center justify-center flex-grow gap-8 bg-gray-100">
+        <div className='w-160'>
+          <div className="w-full max-w-md p-6 rounded-xl">
+            <h1 className="mb-4 text-3xl font-bold">Get Started with <br/>Upstash Workflow</h1>
+            <p>
+              This is a simple example to show how to use Upstash Workflow with Next.js. You can send requests to the workflow by selecting a route and providing a request body.
+            </p>
+            <br/>
+            <p>
+            Each example will have it's own payload structure. To find the related payload type, navigate to the route file in the left sidebar.
+            </p>
+            <br/>
+            <p>
+              After you run the workflow, you can navigate to <a className='font-semibold text-emerald-500' href='https://console.upstash.com/qstash?tab=workflow'>Upstash Console</a> and see the related logs.
+            </p>
+            <Image src="/console.jpeg" alt='upstash console' height={400} width={400} className='mt-4 rounded-xl'/>
+            <div className='flex w-full gap-2 mt-4'>
+            
+            <a className='flex items-center justify-center w-1/2 px-4 py-2 text-white transition-all rounded-md bg-zinc-800 hover:bg-emerald-400' href='https://upstash.com/docs/qstash/workflow/quickstarts/vercel-nextjs'>Docs</a>
+            <a className='flex items-center justify-center w-1/2 gap-2 px-4 py-2 text-white transition-all rounded-md bg-zinc-800 hover:bg-emerald-500' href='https://github.com/upstash/qstash-js/tree/main/examples/workflow/nextjs'>
+            Repository
+            </a>
+            </div>
+
+          </div>
+        </div>
+        <div className="w-full max-w-md p-6 bg-white shadow-md rounded-xl">
+          <h1 className="mb-4 text-xl font-bold">Send Request</h1>
+
+          <div className="mb-4">
+            <label className="block text-gray-700">Route:</label>
+            <select
+              value={route}
+              onChange={(e) => setRoute(e.target.value)}
+              className="block w-full px-3 py-2 mt-1 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            >
+              <option value="" disabled>Select route</option>
+              {routes.map((r) => (
+                <option key={r} value={r}>
+                  {r}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-gray-700">Request Body:</label>
+            <textarea
+              value={requestBody}
+              onChange={(e) => setRequestBody(e.target.value)}
+              className="block w-full px-3 py-2 mt-1 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+
+          <button
+            onClick={handleSend}
+            disabled={loading}
+            className={`w-full px-4 py-2 text-white rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 ${loading ? 'bg-gray-500' : 'bg-emerald-500 hover:bg-emerald-600'}`}
+          >
+            {loading ? 'Sending...' : 'Send'}
+          </button>
+        </div>
+      </div>
+
     </div>
   );
 }
