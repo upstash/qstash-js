2104e09b1869ed1da549c5e6a40cddca0197030d
ytkimirti <yusuftaha9@gmail.com>
2024-09-24 00:30:12 +0300

    chore: clean cf workers example

--- examples/cloudflare-workers/src/index.ts
+++ examples/cloudflare-workers/src/index.ts
@@ -6,7 +6,7 @@ export type Env = {
 };
 
 export default {
-  async fetch(request: Request, env: Env, _ctx: ExecutionContext): Promise<Response> {
+  async fetch(request: Request, env: Env): Promise<Response> {
     const c = new Receiver({
       currentSigningKey: env.QSTASH_CURRENT_SIGNING_KEY,
       nextSigningKey: env.QSTASH_NEXT_SIGNING_KEY,
@@ -18,10 +18,12 @@ export default {
       signature: request.headers.get("Upstash-Signature")!,
       body,
     });
+
+    console.log("isValid", isValid);
+
     if (!isValid) {
       return new Response("Invalid signature", { status: 401 });
     }
-    console.log("was valid");
     return new Response("Hello World!");
   },
 };
