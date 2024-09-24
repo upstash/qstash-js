3a2f54aa7933678ac4c1d9d4c4ed46f1330feeaa
ytkimirti <yusuftaha9@gmail.com>
2024-09-24 11:26:47 +0300

    chore: ignore deprecated warning in test file

--- src/client/llm/chat.test.ts
+++ src/client/llm/chat.test.ts
@@ -14,6 +14,7 @@ async function checkStream(
   stream: AsyncIterable<ChatCompletionChunk>,
   expectInStream: string[] // array of strings to expect in stream
 ): Promise<void> {
+  // eslint-disable-next-line @typescript-eslint/no-deprecated
   const _stream = OpenAIStream(stream);
   const textResponse = new StreamingTextResponse(_stream);
   const text = await textResponse.text();
