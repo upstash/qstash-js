fdb96efe622d2b9015e910ea49cf2a54b070de95
CahidArda <cahidardaooz@hotmail.com>
2025-04-02 11:05:30 +0300

    fix: disable no-explicit-any error in nextjs

--- platforms/nextjs.ts
+++ platforms/nextjs.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-magic-numbers */
 /* eslint-disable unicorn/prevent-abbreviations */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
